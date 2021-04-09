c36095d82393a3d267d2f54c4eb8f46c56ec409a
xiewende <2276230432@qq.com>
2021-04-09 19:15:18 +0800

    Initial commit

---
+++ src/main/java/com/xiewende/creativehomesuppliescity/CreativehomesuppliescityApplication.java
@@ -0,0 +1,13 @@
+package com.xiewende.creativehomesuppliescity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativehomesuppliescityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreativehomesuppliescityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiewende/creativehomesuppliescity/CreativehomesuppliescityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiewende.creativehomesuppliescity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativehomesuppliescityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
